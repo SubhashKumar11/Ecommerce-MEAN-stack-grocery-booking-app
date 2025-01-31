be29f6fb1f96d12f1c8a6617144d917af1cc7cdf
Subhash kumar <113288635+SubhashKumar11@users.noreply.github.com>
2025-01-31 14:20:36 +0530

    Update search-item-list.component.ts

--- Frontend/src/search-item-list/search-item-list/search-item-list.component.ts
+++ Frontend/src/search-item-list/search-item-list/search-item-list.component.ts
@@ -54,23 +54,4 @@ export class SearchItemListComponent implements OnInit {
     });
   }
 }
-/**
- * '@angular/core'; import { FormsModule } from '@angular/forms'; import { CommonModule
-} from '@angular/common'; import { RouterModule } from '@angular/router'; import {
-SearchItemService } from '../servicess/searchItem/search-item.service'; import {
-AddItemCartService } from '../servicess/AddTocart/add-item-cart.service'; import {
-ToastrService } from 'ngx-toastr'; @Component({ selector: 'app-search-item-list',
-standalone: true, imports: [FormsModule,CommonModule,RouterModule], templateUrl:
-'./search-item-list.component.html', styleUrl: './search-item-list.component.css' }) export
-class SearchItemListComponent { searchResults: any[] = []; searchTerm: string = '';
-constructor(private itemService: SearchItemService,private toaster:ToastrService , private
-addToCartService : AddItemCartService) {} onSearch(): void { if (this.searchTerm.trim()) {
-this.itemService.searchItems(this.searchTerm).subscribe( (results) => this.searchResults
-= results, (error) => console.error('Search error:', error) ); } } addItemToCart(item: any) {
-this.addToCartService.addItemToCart(item).subscribe({ next: (res) => { console.log('item
-which is added is:', res); // alert('product added to the cart!'); this.showSuccess(); }, error:
-(error) => { console.log(error); this.showFail(); }, }); } showSuccess() {
-this.toaster.success('item added successfully!', 'Hurray!', { closeButton: true, timeOut:
-10000 }); } showFail() { this.toaster.error('item already is in the cart', 'ohh!', { closeButton:
-true, timeOut:10000 }); } } 
- */
+
